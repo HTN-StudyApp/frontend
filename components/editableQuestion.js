0bc1b3c4b5810d23fddc563fd9b6c4d279540db6
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-18 12:21:50 -0700

    animations on create page

--- components/editableQuestion.js
+++ components/editableQuestion.js
@@ -1,61 +1,92 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faCamera, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faCamera, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import user from '../public/user.png'
+import user from "../public/user.png";
 
 export default function EditableQuestion({ index }) {
+  let [question, setQuestion] = useState("Question here...");
+  // let [choices, setChoices] = useState(["Answer...", "Answer...", "Answer...", "Answer..."])
+  let [choices, setChoices] = useState([0, 1, 2, 3]);
+  let [correct, setCorrect] = useState(0);
+  const [editAnswer, setEditAnswer] = useState(false);
+  const [answer, setAnswer] = useState("answer here...");
 
-    let [question, setQuestion] = useState("Question here...");
-    // let [choices, setChoices] = useState(["Answer...", "Answer...", "Answer...", "Answer..."])
-    let [choices, setChoices] = useState([0, 1, 2, 3])
-    let [correct, setCorrect] = useState(0)
-
-    return (
-        <div className="flex items-center justify-center w-full mb-10">
-            <div className="w-3/4">
-
-                <div className="flex items-center">
-                    <FontAwesomeIcon icon={faCamera} className="w-8 mx-2" />
-                    <span>or, take a picture of the question...</span>
-                </div>
-
-                <div className="flex justify-between text-xl">
-                    <div>
-                        <span>{index + 1}. </span>
-                        <input type="text" value={question}
-                            onChange={(e) => {
-                                setQuestion(e.target.value)
-                            }}
-                            className="focus:outline-none"></input>
-                    </div>
-                    {/* <FontAwesomeIcon icon={faTrashAlt} className="w-8" /> */}
-                </div>
-
-                <div className="flex flex-wrap justify-center items-center">
-                    {choices.map((choice, i) => (
-                        <div key={`${choice} ${correct}`}
-                            style={{ backgroundColor: i == correct ? "green" : "red" }}
-                            className="m-2 rounded-lg w-96 h-16 flex items-center justify-center"
-                            onDoubleClick={() => setCorrect(i)}
-                        >
-                            <input autoFocus type="text" value={choice}
-                                onChange={(e) => {
-                                    let updates = choices;
-                                    updates[i] = e.target.value;
-                                    setChoices([...updates])
-                                }}
-                                className="bg-transparent focus:outline-none"></input>
-                        </div>
-                    ))}
-                </div>
-
+  return (
+    <div className="flex flex-col items-center justify-center w-full mb-10">
+      <div
+        className="w-3/5 h-[36rem] relative group"
+        style={{ perspective: "400rem" }}
+      >
+        <div
+          className={`absolute flex flex-col inset-0 ${
+            editAnswer ? "-rotate-y-180" : ""
+          } duration-500`}
+          style={{ backfaceVisibility: "hidden" }}
+        >
+          <div className={"flex justify-between items-end"}>
+            <h2 className={"text-2xl mt-6"}>Question {index + 1}</h2>
+            <button onClick={() => setEditAnswer(true)}>Edit answer</button>
+          </div>
+          <div
+            className={
+              "mt-2 shadow-lg w-full flex-grow bg-white rounded-md flex items-center justify-center"
+            }
+          >
+            <div className={"px-16 py-16 text-3xl w-full flex items-center"}>
+              <span>{index + 1}. </span>
+              <input
+                type="text"
+                value={question}
+                onChange={(e) => {
+                  setQuestion(e.target.value);
+                }}
+                className="text-2xl flex-grow border-0 focus:border-0 focus:outline-none focus:ring-0"
+              />
             </div>
+          </div>
         </div>
-    )
+        <div
+          className={`absolute inset-0 flex flex-col rotate-y-180 ${
+            editAnswer ? "rotate-y-0" : ""
+          } duration-500`}
+          style={{ backfaceVisibility: "hidden" }}
+        >
+          <div className={"flex justify-between items-end"}>
+            <h2 className={"text-2xl mt-6"}>Answer {index + 1}</h2>
+            <button onClick={() => setEditAnswer(false)}>Edit question</button>
+          </div>
+          <div
+            className={
+              "mt-2 shadow-lg w-full flex-grow bg-white rounded-md flex items-center justify-center"
+            }
+          >
+            <div className={"px-16 py-16 text-3xl w-full flex items-center"}>
+              <span>{index + 1}. </span>
+              <input
+                type="text"
+                value={answer}
+                onChange={(e) => {
+                  setAnswer(e.target.value);
+                }}
+                className="text-2xl flex-grow border-0 focus:border-0 focus:outline-none focus:ring-0"
+              />
+            </div>
+          </div>
+        </div>
+        {/* <FontAwesomeIcon icon={faTrashAlt} className="w-8" /> */}
+      </div>
+      <div className={"mt-8"}>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faCamera} className="w-6 text-gray-600 mx-2" />
+          <span>or, take a picture of the question...</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/create.js
+++ pages/create.js
@@ -1,50 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from "../components/header";
+import Footer from "../components/footer";
 
-import { useState, useEffect } from 'react';
-import EditableQuestion from '../components/editableQuestion';
+import { useState, useEffect } from "react";
+import EditableQuestion from "../components/editableQuestion";
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function Create() {
+  let [setName, setSetName] = useState("Untitled Set");
+  let [questions, setQuestions] = useState(1);
 
-    let [setName, setSetName] = useState("Untitled Set");
-    let [questions, setQuestions] = useState(1);
+  return (
+    <div className={"bg-gray-100"}>
+      <Head>
+        <title>Study App</title>
+        <meta name="description" content="HTN Study App" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
 
-    return (
-        <div >
-            <Head>
-                <title>Study App</title>
-                <meta name="description" content="HTN Study App" />
-                <link rel="icon" href="/logo.png" />
-            </Head>
+      <Header />
 
-            <Header />
+      <main
+        style={{ marginTop: "60px", minHeight: "100vh" }}
+        className="flex flex-col items-center"
+      >
+        <input
+          type="text"
+          value={setName}
+          onChange={(e) => setSetName(e.currentTarget.value)}
+          className="bg-transparent outline-none border-0 focus:ring-0 focus:outline-none text-5xl w-3/4"
+        ></input>
 
-            <main style={{ marginTop: "60px", minHeight: "100vh" }}
-                className="flex flex-col items-center"
-            >
-                <input type="text" value={setName}
-                    onChange={e => setSetName(e.currentTarget.value)}
-                    className="focus:outline-none text-5xl w-3/4"></input>
+        {new Array(questions).fill(0).map((x, i) => (
+          <EditableQuestion key={`${i} ${questions}`} index={i} />
+        ))}
+      </main>
 
-                {new Array(questions).fill(0).map((x, i) => (
-                    <EditableQuestion key={`${i} ${questions}`} index={i} />
-                ))}
-
-                <div className="w-12 h-12 rounded-full bg-red-100 flex items-center justify-center mb-10"
-                    onClick={() => setQuestions(questions + 1)}>
-                    <FontAwesomeIcon icon={faPlus} className="m-2" />
-                </div>
-
-            </main>
-
-            <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
