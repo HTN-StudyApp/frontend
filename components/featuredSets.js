e12ec7525f4d9a7d30ba222a9815a316eb493ac7
Mayank Kumar <kmayank38@gmail.com>
2021-09-19 11:30:55 +0530

    updating sets

--- components/featuredSets.js
+++ components/featuredSets.js
@@ -1,20 +1,20 @@
-import Link from 'next/link'
-import List from './list'
+import Link from "next/link";
+import List from "./list";
 
-export default function FeaturedSets({ }){
-    return(
-        <div>
-            <div style={{display:"flex", height:"50px"}}>    
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-                <List/>
-            </div>
-        </div>
-    )
+export default function FeaturedSets({}) {
+  return (
+    <div>
+      <div style={{ display: "flex", height: "50px" }}>
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+      </div>
+    </div>
+  );
 }
